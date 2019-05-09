51211eca4eba8d47a142c29a77a5dcc7861fb88e
Nathan Scheufele <38326749+NathansLab@users.noreply.github.com>
2019-05-09 18:20:10 +0200

    Bugfixes

--- 8266_blynk.ino
+++ 8266_blynk.ino
@@ -1,7 +1,5 @@
 #define BLYNK_PRINT Serial
 
-#include <FastLED.h>
-#include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -9,23 +7,22 @@ BlynkTimer timer;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(60, 2, NEO_GRBW + NEO_KHZ800);
 
 int white, red, green, blue, modus;
-int b = 39;
-int w = 15;
-int p = 3;
+int b = 0;
+float posnow, posthen;
 int z = 1;
-int statenow[61][4];
-int statethen[61][4];
-int now;
+float statenow[61][4];
+float statethen[61][4];
 float spe = 0.1;
+int now, last;
 
 void setup()
 {
   Serial.begin(230400);
   pixels.begin();
   Blynk.begin("78ba576f1d0e47489a2368cc50678032", "FRITZ!Box Scheufele", "Scheufele2016", "scheufele.ddns.net", 8080);
-  timer.setInterval(30L, updateLED);
-  timer.setInterval(100L, colorcalc);
-  timer.setInterval(30L, transition);
+  timer.setInterval(15L, updateLED);
+  timer.setInterval(15L, colorcalc);
+  timer.setInterval(15L, transition);
 }
 
 void colorcalc() {
@@ -33,23 +30,26 @@ void colorcalc() {
     float fred = (float)red / 255;
     float fgreen = (float)green / 255;
     float fblue = (float)blue / 255;
-    //Serial.println("calc");
+    float fwhite = (float)white / 255;
     for (int i = 0; i < pixels.numPixels(); i++)
     {
-      z = abs(1 + (1) ^ ((i - p) ^ 2));
+      posnow = posnow + spe * 0.1 * (posthen - posnow);
+      z = abs(1 + (1) ^ ((i - (int)posnow) ^ 2));
       if (z == 0) {
         z = 1;
       }
+
       float temp = b / z;
-      statethen[i][0] = temp * fred;
-      statethen[i][1] = temp * fgreen;
-      statethen[i][2] = temp * fblue;
+
+      statethen[i][0] = temp * fred * (b/255);
+      statethen[i][1] = temp * fgreen * (b/255);
+      statethen[i][2] = temp * fblue * (b/255);
+      statethen[i][3] = temp * fwhite * (b/255);
     }
   }
 }
 
 void transition() {
-  //Serial.println("transitoin");
   for (int i = 0; i < pixels.numPixels(); i++)
   {
     statenow[i][0] = statenow[i][0] + spe * ((float)statethen[i][0] - (float)statenow[i][0]);
@@ -60,19 +60,16 @@ void transition() {
 }
 
 void updateLED() {
-  //Serial.println("updateLED");
-  if (modus == 2) {
+  now = micros();
+  Serial.println(now - last);
+  last = now;
+  if ((modus == 3) || (modus == 2)) {
     for (int i = 0; i < pixels.numPixels(); i++) {
       pixels.setPixelColor(i, pixels.Color(statenow[i][0], statenow[i][1], statenow[i][2], statenow[i][3]));
     }
     pixels.show();
   }
-  if (modus == 3) {
-    for (int i = 0; i < pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, pixels.Color(statenow[i][0], statenow[i][1], statenow[i][2], statenow[i][3]));
-    }
-    pixels.show();
-  }
+  //if (((statenow[3][0]==0)&&(statenow[3][1]==0)&&(statenow[3][2]==0)){}
 }
 
 void loop()
@@ -91,9 +88,13 @@ BLYNK_WRITE(V0)
   if (modus == 1) {
     for (int i = 0; i < pixels.numPixels(); i++)
     {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0, 0));
+      statethen[i][0] = 0;
+      statethen[i][1] = 0;
+      statethen[i][2] = 0;
+      statethen[i][3] = 0;
+      //pixels.setPixelColor(i, pixels.Color(0, 0, 0, 0));
     }
-    pixels.show();
+    //pixels.show();
   }
 }
 
@@ -127,15 +128,11 @@ BLYNK_WRITE(V3)
 BLYNK_WRITE(V4)
 {
   white = param.asInt();
-  for (int i = 0; i < pixels.numPixels(); i++)
-  {
-    statethen[i][3] = white;
-  }
 }
 
 BLYNK_WRITE(V5)
 {
-  spe = (float)param.asInt() / 100;
+  spe = (float)param.asInt() / 1000;
 }
 
 BLYNK_WRITE(V6)
@@ -145,10 +142,5 @@ BLYNK_WRITE(V6)
 
 BLYNK_WRITE(V7)
 {
-  p = param.asInt();
-}
-
-BLYNK_WRITE(V8)
-{
-  w = param.asInt();
+  posthen = param.asInt();
 }
